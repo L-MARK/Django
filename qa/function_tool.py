155da879e7a564f3127d757c9547ca0b9d9489f6
L-MARK <168156160+L-MARK@users.noreply.github.com>
2024-09-10 17:30:17 +0800

    图片描述bug与新界面不匹配修复+ppt格式兼容

--- qa/function_tool.py
+++ qa/function_tool.py
@@ -2,6 +2,7 @@ import base64
 from typing import Callable, List, Dict, Tuple
 import time
 import json
+import json5
 from client.clientfactory import Clientfactory
 from zhipuai import ZhipuAI
 from qa.purpose_type import userPurposeType
@@ -111,10 +112,10 @@ def process_images_tool(question_type, question, history, image_url=None):
 
 
 def process_image_describe_tool(question_type, question, history, image_url=None):
-    if question is None:
+    if len(question)==0:
         question = "描述这个图片，说明这个图片的主要内容"
 
-    img_path = image_url
+    img_path = image_url[0]
     client = Clientfactory.get_special_client(client_type=question_type)
     if is_file_path(img_path):
         with open(img_path, "rb") as img_file:
@@ -155,7 +156,7 @@ def process_ppt_tool(
     question_type, question: str, history: List[List[str] | None] = None, image_url=None
 ) -> Tuple[Tuple[str, str], userPurposeType]:
     raw_text: str = generate_ppt_content(question, history)
-    ppt_content = json.loads(raw_text)
+    ppt_content = json5.loads(raw_text)
     ppt_file: str = generate_ppt(
         ppt_content
     )  # 这个语句由于模型能力有限，可能不会按照格式输出，会导致冲突，要用str正则语句修改，删除一些异常符号，否则会出bug
